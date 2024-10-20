9b5662758c67aa3b9b4fe5600e8b774eaad046fe
kokk <kokk7310@gmail.com>
2024-10-21 01:11:03 +0900

    테스트 푸시

--- bot.py
+++ bot.py
@@ -19,6 +19,7 @@ def build_message(item):
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
